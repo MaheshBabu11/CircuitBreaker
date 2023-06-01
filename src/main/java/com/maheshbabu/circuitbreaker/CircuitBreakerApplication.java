7878ad270a291c4c0c394403566c731d87dea887
Mahesh Babu <mahesh.b.pec@gmail.com>
2023-06-01 15:01:48 +0530

    First commit

---
+++ src/main/java/com/maheshbabu/circuitbreaker/CircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.maheshbabu.circuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerApplication.class, args);
+	}
+
+}
